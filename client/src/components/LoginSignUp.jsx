15c11e89e1d743a8ce836501f5a72a4b6d41f12f
NaufalGhifari <85378958+NaufalGhifari@users.noreply.github.com>
2023-04-17 00:05:48 +0800

    styled login buttons

--- client/src/components/LoginSignUp.jsx
+++ client/src/components/LoginSignUp.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 function LoginSignUp(){
     return(
-        <div className=' text-centered'>
+        <div className='login-signup-div text-centered'>
             <p>Please log in to start talking about the weather. Don't have an account? Sign up.</p>
             <ul className='sign-log-in-buttons-list  text-centered'>
-                <li><button>Log In</button></li>
-                <li><button>Sign Up</button></li>
+                <li><button id='login-button'>Log In</button></li>
+                <li><button id='signup-button'>Sign Up</button></li>
             </ul>
             
         </div>
